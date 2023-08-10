6042f3f140bd98cb1ccec591a2992e9fee716c83
Eunice-4 <umoreneunice4@gmail.com>
2023-08-10 16:32:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A program that prints a line to the standard error
- * Return: 1 (Success)
+ * main - Entry point of the program
+ *
+ * Description: Prints a line to the standard output
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
+
