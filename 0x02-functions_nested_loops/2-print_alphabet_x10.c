4279f2501f0ebd8456cc72d5cbac8771e9e2a369
Eunice-4 <umoreneunice4@gmail.com>
2023-08-15 10:21:20 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - prints alphabets 10 times
  * Return: void
  */
+
 void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
+
+	i = 0;
 
 	while (i < 10)
 	{
