3a25116fbdc967ab9253cc69544464826dc861c8
Eunice-4 <umoreneunice4@gmail.com>
2023-08-15 10:56:52 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,14 +3,17 @@
 /**
  * _islower - checks for lower case characters
  * @c: The character to be checked
- * Return: 1 for lowercase characters, 0 for anything else
+ * Return: 1 for lowercase characters or 0 for anything else
  */
 
 int _islower(int c)
+
 {
+
 	if (c <= 97 && c >= 122)
 	{
-		return (1);
+
+	return (1);
 	}
 	return (0);
 }
