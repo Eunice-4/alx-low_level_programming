cea9ab7917db5a7db2a2ab25a2ea31d38a64e17f
Eunice-4 <umoreneunice4@gmail.com>
2023-08-16 10:02:37 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,24 +3,27 @@
 /**
  * print_sign - prints the sign of a number
  * @n: The integer to be checked
- * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
+ * Return: 1 for positive num, -1 for negative number, 0 for anything else
  */
+
 int print_sign(int n)
+
 {
+
 	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
+	_putchar('+');
+	return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
-		return (-1);
+	_putchar('-');
+	return (-1);
 	}
 	else
 	{
-		_putchar('0');
-		return (0);
+	_putchar('0');
+	return (0);
 	}
 }
 
