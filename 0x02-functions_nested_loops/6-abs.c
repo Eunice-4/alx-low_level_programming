7f42961289e2fe5bcd33af6172aa1febda23fc42
Eunice-4 <umoreneunice4@gmail.com>
2023-08-17 07:50:24 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ *@r: Integer to be checked
+ * Return: 0
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+
+		return (r);
+	else
+		return (r = r * -1);
+}
