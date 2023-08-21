a46b77bb41dad4a96eb2baa2a7714670bea1a5c5
Eunice-4 <umoreneunice4@gmail.com>
2023-08-21 02:57:14 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,14 @@
 
 /**
  * print_numbers - prints the numbers from 0 to 9
- * Return: Always 0
+ * Return: No return
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 	}
