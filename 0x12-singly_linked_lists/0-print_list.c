92f5c0ab3c4f22cb6bffe461ee11d6edaf5cab3e
Eunice-4 <umoreneunice4@gmail.com>
2023-09-22 08:56:53 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements in a linked list
+ * Return: number of nodes
+ * @h: pointer to next node
+ */
+size_t print_list(const list_t *h)
+{
+	size_t no_nodes = 0;
+
+	if (h == NULL)
+	{
+		printf("[0] (nil)");
+		no_nodes++;
+	}
+	while (h != NULL)
+	{
+		printf("[%u] %s\n", h->len, h->str);
+		no_nodes++;
+		h = h->next;
+	}
+	printf("\n");
+	return (no_nodes);
+}
