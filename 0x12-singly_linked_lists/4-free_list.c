2b0ffad1588130da63556c52bb413eab36ad8fe8
Eunice-4 <umoreneunice4@gmail.com>
2023-09-23 05:51:48 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list_t list
+ */
+void free_list(list_t *head)
+{
+	list_t *current, *temp;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp->str);
+		free(temp);
+	}
+}
