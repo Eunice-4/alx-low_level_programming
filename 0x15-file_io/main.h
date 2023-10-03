525929f65591e3d98ef7a09c644e3da99a58b86c
Eunice-4 <umoreneunice4@gmail.com>
2023-10-02 17:55:54 -0700

    1-create_file.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,9 +2,11 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 #ifndef MAIN_H
 #define MAIN_H
 
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
