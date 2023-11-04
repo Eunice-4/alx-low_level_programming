9f309f80323050116fc92eb9e151f381b61cb059
Eunice-4 <138872340+Eunice-4@users.noreply.github.com>
2023-11-04 12:23:02 +0000

    Sat, Nov 4, 2023, 1:23 PM +01:00

--- assignments/assignment1.c
+++ assignments/assignment1.c
@@ -8,6 +8,10 @@ int main(void)
     fopen(numbers.txt, RDONLY);
     fread(buffer, sizeof(int), sizeof(buffer), numbers.txt);
 
-    sum = buffer[0 ]
+    sum = buffer[0] + buffer[1] + buffer[2] + buffer[3] + buffer[4] + buffer[5];
+    printf("Sum of the numbers: %d\n", sum);
+
+    fclose(numbers.txt)
+    return(0);
 
 }
